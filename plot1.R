483005049903f0bf76a88bb499f6e2c64f386b39
hufeiling <hufeiling@gmail.com>
2016-05-10 09:42:48 -0500

    Add comments for Plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,15 @@
+# Set working directory and read data
+
 setwd("C:/Users/hufeiling/learning/datascience/datasciencecoursera/Class4ExploratoryAnalysis/Week1/Project")
 HouseholdData<-read.table("HouseholdData/household_power_consumption.txt",sep=";",header=T)
+
+# Subset data
+
 ProjectData<-HouseholdData[(as.Date(HouseholdData$Date,"%d/%m/%Y")>="2007-02-01"&as.Date(HouseholdData$Date, "%d/%m/%Y")<="2007-02-02"),]
 Plot1Data<-ProjectData[ProjectData$Global_active_power!="?",]
-png(filename="GitHub/plot1.png")
+
+#Plot 1
+
+png(filename="GitHub/plot1.png", width = 480, height = 480, units = "px")
 hist(as.numeric(as.character(Plot1Data$Global_active_power)),col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
 dev.off()
